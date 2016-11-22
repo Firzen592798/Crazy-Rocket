bd743c2d5aa750dba9fc7226601b59915d4917b9
Firzen592798 <tyranitar_nb@hotmail.com>
2016-11-22 00:14:13 -0300

    Adicionando a barra de impulso

--- Assets/Scripts/FogueteController.cs
+++ Assets/Scripts/FogueteController.cs
@@ -8,28 +8,35 @@ public class FogueteController : MonoBehaviour {
 	private bool _moving = false;
 	private Vector3 _targetPos;
 
-	public float vida;
-	public float vidaCheia;
-	public Texture contorno;
+	private float impulsoAtual = 8;
+	public float impulsoCheio = 10;
+	public Texture contorno, barraImpulso;
 
-	void OnGui(){
-		GUI.DrawTexture (new Rect (Screen.width / 40, Screen.height / 40, Screen.width / 5, Screen.height / 8), contorno);
+	void OnGUI(){
+		GUI.DrawTexture (new Rect (Screen.width / 27, Screen.height / 25, Screen.width / 8.5f/impulsoCheio*impulsoAtual, Screen.height / 25), barraImpulso);
+		GUI.DrawTexture (new Rect (Screen.width / 30, Screen.height / 40, Screen.width / 8, Screen.height / 15), contorno);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+		impulsoAtual = 10 * (1 - Mathf.Max(_nextImpulso - Time.time, 0));
+		print(10 * (1 - Mathf.Max(_nextImpulso - Time.time, 0)));
 		if (Input.GetMouseButton(0)) {
 			if (Time.time > _nextImpulso) {
 				_targetPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 				_targetPos.z = transform.position.z;
 				transform.GetComponent<Rigidbody2D> ().velocity = Vector3.zero;
-				print (moveSpeed);
+
 				transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(_targetPos.x - transform.position.x, _targetPos.y - transform.position.y) * moveSpeed);
 				_nextImpulso = Time.time + tempoImpulso;
 				//transform.position = Vector3.MoveTowards (transform.position, _targetPos, moveSpeed * Time.deltaTime);
 
 			}
+		}
+		if (impulsoAtual >= impulsoCheio) {
+			impulsoAtual = impulsoCheio;
+		} else if (impulsoAtual <= 0) {
+			impulsoAtual = 0;
 		}
 	}
 
