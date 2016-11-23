994ee9838a47e5f5e04c987fb524c2fcd25d4adb
Firzen592798 <tyranitar_nb@hotmail.com>
2016-11-23 00:40:12 -0300

    adicionando menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+	public TextMesh jogar;
+	public TextMesh sobre;
+	// Use this for initialization
+	void Start () {
+		jogar = jogar.GetComponent<TextMesh> ();
+		sobre = sobre.GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Jogar(){
+		Application.LoadLevel ("Main");
+	}
+	public void Sobre(){
+		Application.LoadLevel ("Sobre");
+	}
+}
