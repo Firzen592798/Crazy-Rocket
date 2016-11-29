aa93ae93cb2aa01bf0ce568961b072febb24cb14
Fernando <fernandocdds@hotmail.com>
2016-11-29 11:17:05 -0300

    Imagens de movimento
    Inclui novas sprites para outros movimentos.

--- Assets/TrocandoSprites.cs
+++ Assets/TrocandoSprites.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class TrocandoSprites : MonoBehaviour {
 
-	public Sprite spriteparado; // Drag your first sprite here
-	public Sprite spritesubindo; // Drag your second sprite here
-	public Sprite spritedescendo; // Drag your second sprite here
+	public Sprite spriteparado; 
+	public Sprite spritesubindo; 
+	public Sprite spritedescendo; 
+	public Sprite spritefrentedescendo;
+	public Sprite spritefrentesubindo;
+	public Sprite spritetrasdescendo;
+	public Sprite spritetrassubindo;
+	public Sprite spritefreando;
+
+
 
 	private SpriteRenderer spriteRenderer; 
 
@@ -16,40 +23,63 @@ public class TrocandoSprites : MonoBehaviour {
 			spriteRenderer.sprite = spriteparado; // set the sprite to sprite1
 	}
 
-	//void Update ()
-	//{
-	//	if (Input.GetKeyDown (KeyCode.UpArrow)) // If the space bar is pushed down
-	//	{
-	//		Subindo (); // call method to change sprite
-	//	}
-	//	if (Input.GetKeyDown (KeyCode.DownArrow)) // If the space bar is pushed down
-	//	{
-	//		Descendo (); // call method to change sprite
-	//	}
-	//}
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.UpArrow)) // If the space bar is pushed down
+		{
+			Subindo (); // call method to change sprite
+		}
+		if (Input.GetKeyDown (KeyCode.DownArrow)) // If the space bar is pushed down
+		{
+			Descendo (); // call method to change sprite
+		}
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) // If the space bar is pushed down
+		{
+			Freando (); // call method to change sprite
+		}
+		if (Input.GetKeyDown (KeyCode.PageUp)) // If the space bar is pushed down
+		{
+			FrenteSubindo (); // call method to change sprite
+		}
+		if (Input.GetKeyDown (KeyCode.PageDown)) // If the space bar is pushed down
+		{
+			FrenteDescendo (); // call method to change sprite
+		}
+		if (Input.GetKeyDown (KeyCode.Home)) // If the space bar is pushed down
+		{
+			TrazSubindo (); // call method to change sprite
+		}
+		if (Input.GetKeyDown (KeyCode.End)) // If the space bar is pushed down
+		{
+			TrazDescendo (); // call method to change sprite
+		}
+
+	}
 
 	public void Subindo ()
 	{
-		if (spriteRenderer.sprite == spriteparado) // if the spriteRenderer sprite = sprite1 then change to sprite2
-		{
+		//if (spriteRenderer.sprite == spriteparado) // if the spriteRenderer sprite = sprite1 then change to sprite2
+		//{
 			spriteRenderer.sprite = spritesubindo;
-		}
-		else
-		{
-			spriteRenderer.sprite = spriteparado; // otherwise change it back to sprite1
-		}
+		//}
+		//else
+		//{
+		//	spriteRenderer.sprite = spriteparado; // otherwise change it back to sprite1
+		//}
 	}
 
 	public void Descendo ()
 	{
-		if (spriteRenderer.sprite == spriteparado) // if the spriteRenderer sprite = sprite1 then change to sprite2
-		{
+		//if (spriteRenderer.sprite == spriteparado) // if the spriteRenderer sprite = sprite1 then change to sprite2
+		//{
 			spriteRenderer.sprite = spritedescendo;
-		}
-		else
-		{
-			spriteRenderer.sprite = spriteparado; // otherwise change it back to sprite1
-		}
+		//}
+		//else
+		//{
+			//spriteRenderer.sprite = spriteparado; // otherwise change it back to sprite1
+		//}
 	}
 
 	public void Parado ()
@@ -57,4 +87,35 @@ public class TrocandoSprites : MonoBehaviour {
 		spriteRenderer.sprite = spriteparado; // otherwise change it back to sprite1
 		
 	}
+
+	public void Freando ()
+	{
+		spriteRenderer.sprite = spritefreando; // otherwise change it back to sprite1
+
+	}
+
+	public void FrenteSubindo ()
+	{
+		spriteRenderer.sprite = spritefrentesubindo; // otherwise change it back to sprite1
+
+	}
+
+	public void FrenteDescendo ()
+	{
+		spriteRenderer.sprite = spritefrentedescendo; // otherwise change it back to sprite1
+
+	}
+
+	public void TrazSubindo ()
+	{
+		spriteRenderer.sprite = spritetrassubindo; // otherwise change it back to sprite1
+
+	}
+
+	public void TrazDescendo ()
+	{
+		spriteRenderer.sprite = spritetrasdescendo; // otherwise change it back to sprite1
+
+	}
+
 }
